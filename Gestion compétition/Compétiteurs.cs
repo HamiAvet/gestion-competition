889eb43deed5e242258c420bced1dc34704feba8
HamiAvet <hamov2003@gmail.com>
2026-05-11 17:32:09 +0200

    first commit

---
+++ Gestion compétition/Compétiteurs.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_compétition
+{
+    // internal class Compétiteur par défaut
+    // On rend la classe Compétiteur abstraite pour éviter qu'elle ne soit instanciée directement, elle doit être héritée par les classes Combattant et Choregraphe
+    public abstract class Competiteur
+    {
+        // Les attributs - Propriétés
+        private string nom;
+        private string prenom;
+        private int age;
+        private string nationalite;
+
+        // Les constructeurs de la classe Compétiteur
+        // Sans pièce d'identité
+
+        // Constructeur protégé pour éviter qu'il ne soit utilisé directement, il doit être appelé par les classes filles
+        protected Competiteur(string prenom, string nom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+
+        // Avec pièce d'identité
+
+        // Constructeur protégé pour éviter qu'il ne soit utilisé directement, il doit être appelé par les classes filles
+        protected Competiteur(string prenom, string nom, int age, string nationalite)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            this.nationalite = nationalite;
+        }
+        
+        // Les classes filles de la classe Compétiteur
+        public class Combattant : Competiteur
+        {
+            // Attributs spécifiques à la classe Combattant
+            private int poids;
+
+            // Constructeur de la classe Combattant
+            public Combattant(string prenom, string nom, int age, int poids, string nationalite = null) : base(prenom, nom, age, nationalite)
+            {
+                this.poids = poids;
+            }
+
+            // Méthode pour afficher les informations du combattant
+            public override string InfoCompetiteur()
+            {
+                return (RecupInfo() + " Pesant " + poids + " kg.");
+            }
+        }
+
+        public class Choregraphe : Competiteur
+        {
+            // Attributs spécifiques à la classe Choregraphe
+            private string styleDanse;
+
+            // Constructeur de la classe Choregraphe
+            public Choregraphe(string prenom, string nom, int age, string styleDanse, string nationalite = null) : base(prenom, nom, age, nationalite)
+            {
+                this.styleDanse = styleDanse;
+            }
+
+            // Méthode pour afficher les informations du chorégraphe
+            public override string InfoCompetiteur()
+            {
+                return (RecupInfo() + " Spécialisé(e) dans le style de danse " + styleDanse + ".");
+            }
+        }
+
+        // Méthode pour définir la nationalité du compétiteur
+        public void SetNationalite(string nationalite)
+        {
+            this.nationalite = nationalite;
+        }
+
+        // Méthode pour récupérer le nom du compétiteur
+        public string getNom()
+        {
+            return nom;
+        }
+
+        // Méthode pour afficher les informations du compétiteur générique
+        protected string RecupInfo()
+        {
+            // Affichage des informations du compétiteur générique en fonction de la présence ou non de la nationalité
+            if (nationalite != null)
+            {
+                return ("Le compétiteur " + prenom + " " + nom + " agé(e) de " + age + " ans, de nationalité " + nationalite + ".");
+            }
+            else
+            {
+                return ("Le compétiteur " + prenom + " " + nom + " agé(e) de " + age + " ans.");
+            }
+        }
+
+        public abstract string InfoCompetiteur();
+    }
+}
---
+++ Gestion compétition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_compétition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création d'un compétiteur de type Combattant
+            Competiteur.Combattant competiteur2 = new Competiteur.Combattant("Gérard", "Bouchard", 55, 105, "française");
+
+            // Création d'un compétiteur de type Choregraphe
+            Competiteur.Choregraphe competiteur3 = new Competiteur.Choregraphe("Sandrine", "Cendrier", 35, "hip-hop");
+
+            List<Competiteur> listeCompetiteurs = new List<Competiteur> { competiteur2, competiteur3 };
+
+            foreach (Competiteur competiteur in listeCompetiteurs)
+            {
+                Console.WriteLine(competiteur.InfoCompetiteur());
+            }
+
+            // Afficher la console pour éviter qu'elle ne se ferme immédiatement
+            Console.ReadKey();
+        }
+    }
+}
